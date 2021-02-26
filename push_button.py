679c14c0f71977bc1dfa1cbbad3b2107da5558a7
Eric Bonvin <eric.bonvin@me.com>
2021-02-26 20:01:01 +0100

    Testing pushbutton

--- push_button.py
+++ push_button.py
@@ -25,8 +25,7 @@ class PushButton:
         self._last_value_time = 0
         self.memory = 5.
         self.long_threshold = long_threshold
-        self._button_up = True
-        self._timeout = 10.  # button timeout
+        self._debounce = 0.5  # button_up debounce
 
     @property
     def button_pushed(self):
@@ -38,13 +37,14 @@ class PushButton:
 
     def button_readout(self, channel):
         log.debug('Enter button readout')
-        self._button_up = False
+        if time.time() - self._last_value_time > self._debounce:
+            log.debug('Debouncing on button up')
+            return
+
         start_time = time.time()
 
-        while not self._button_up: # Wait for the button up
-            if time.time() - start_time > self._timeout:
-                log.debug('Button timeout!')
-                return
+        while GPIO.input(channel) == 0: # Wait for the button up
+            pass
 
         buttonTime = time.time() - start_time    # How long was the button down?
         log.debug('Button up. press length: {:.1f}'.format(buttonTime))
@@ -56,8 +56,5 @@ class PushButton:
             self._button_pushed = 2  # 2 Long press
             log.debug('was long')
 
-        self._last_value_time = time.time()  # remember when the last event happend
+        self._last_value_time = time.time()  # remember when the last event happened
         log.debug('Exit button push')
-
-    def button_up(self):
-        self._button_up = True
