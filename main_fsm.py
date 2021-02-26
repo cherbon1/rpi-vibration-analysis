c808548bc0287c31288bff37457d9d2396a57f07
Eric Bonvin <eric.bonvin@me.com>
2021-02-26 19:51:32 +0100

    Fix push button

--- main_fsm.py
+++ main_fsm.py
@@ -79,8 +79,8 @@ while True:
         indicator_lights.ready()
 
         if push_button.button_pushed == 2:
-            log.debug('Would shutdown now. skipped instead')
-            # next_state = VibrationStationStates.SHUTDOWN
+            # log.debug('Would shutdown now. skipped instead')
+            next_state = VibrationStationStates.SHUTDOWN
             continue
 
         if GPIO.input(manual_switch_pin) == GPIO.HIGH:
